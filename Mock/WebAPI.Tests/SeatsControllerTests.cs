a72832cad2bc3c25bbe9f32fcf50da87d4106d92
j <2282988@cegepmontpetit.ca>
2025-05-16 10:23:30 -0400

    changement de l'exercice mock

--- Mock/WebAPI.Tests/SeatsControllerTests.cs
+++ Mock/WebAPI.Tests/SeatsControllerTests.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System.Security.Claims;
 using WebAPI.Controllers;
 using WebAPI.Exceptions;
 using WebAPI.Models;
@@ -12,109 +12,61 @@ namespace WebAPI.Tests;
 [TestClass]
 public class SeatsControllerTests
 {
-    [TestMethod]
-    public void ReserveSeat()
+    Mock<SeatsService> servicemock;
+    Mock<SeatsController> controllermock;
+
+    public SeatsControllerTests()
     {
-        //mock du service (en premier pour pouvoir l'injecter dans le construteur du controlleur)
-        Mock<SeatsService> ServiceMock = new Mock<SeatsService>();
+        servicemock = new Mock<SeatsService>();
+        controllermock = new Mock<SeatsController>(servicemock.Object) { CallBase = true };
 
-        //Mock du controller
-        Mock<SeatsController> ControllerMock = new Mock<SeatsController>(ServiceMock.Object) { CallBase = true };
-
-        //configuration de la propriete userid
-        ControllerMock.Setup(t => t.UserId).Returns("1");
+        //Setup du userID
+        controllermock.Setup(t => t.UserId).Returns("12345");
+    }
 
 
-        //creer un seat
-        Seat seat = new Seat() { Id = 1 };
+    [TestMethod]
+    public void ReserveSeat() 
+    { 
 
-        //configuration de la reservation du seat
-        ServiceMock.Setup(s => s.ReserveSeat("1", 1)).Returns(seat);
+        Seat seat = new Seat() { Id = 1, Number = 1 };
 
-        //configuration de la reservation du seat avec le service 
-        var actionresult = ControllerMock.Object.ReserveSeat(1);
-        
-        //resulat attendu
+        servicemock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Returns(seat);
+
+        var actionresult = controllermock.Object.ReserveSeat(seat.Number);
         var result = actionresult.Result as OkObjectResult;
         Assert.IsNotNull(result);
     }
 
+
     [TestMethod]
     public void ReserveSeatUnauthorized()
     {
-        //mock du service (en premier pour pouvoir l'injecter dans le construteur du controlleur)
-        Mock<SeatsService> ServiceMock = new Mock<SeatsService>();
+        servicemock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new SeatAlreadyTakenException());
 
-        //Mock du controller
-        Mock<SeatsController> ControllerMock = new Mock<SeatsController>(ServiceMock.Object) { CallBase = true };
-
-        //configuration de la propriete userid
-        ControllerMock.Setup(t => t.UserId).Returns("1");
-
-        //configuration de la reservation du seat pour throw une exception donc
-        //peut importe les parametre on veut juste voir si cv retourner Unauthorized à la fin
-        ServiceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new SeatAlreadyTakenException()); ;
-
-        //configuration de la reservation du seat avec le service 
-        var actionresult = ControllerMock.Object.ReserveSeat(1);
-
-        //resulat attendu
+        var actionresult = controllermock.Object.ReserveSeat(1);
         var result = actionresult.Result as UnauthorizedResult;
         Assert.IsNotNull(result);
     }
 
     [TestMethod]
-    public void ReserveSeatNotFound()
+    public void NotFound()
     {
-        //mock du service (en premier pour pouvoir l'injecter dans le construteur du controlleur)
-        Mock<SeatsService> ServiceMock = new Mock<SeatsService>();
+        servicemock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new SeatOutOfBoundsException());
 
-        //Mock du controller
-        Mock<SeatsController> ControllerMock = new Mock<SeatsController>(ServiceMock.Object) { CallBase = true };
-
-        //configuration de la propriete userid
-        ControllerMock.Setup(t => t.UserId).Returns("1");
-
-        //creer un seat
-        Seat seat = new Seat() { Number = 4 };
-
-        //configuration de la reservation du seat pour throw une exception donc
-        //peut importe les parametre on veut juste voir si cv retourner Unauthorized à la fin
-        ServiceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new SeatOutOfBoundsException()); 
-
-        //configuration de la reservation du seat avec le service 
-        var actionresult = ControllerMock.Object.ReserveSeat(seat.Number);
-
-        //resulat attendu
+        var actionresult = controllermock.Object.ReserveSeat(1);
         var result = actionresult.Result as NotFoundObjectResult;
         Assert.IsNotNull(result);
-
-        //verifier que les message sont identique
-        Assert.AreEqual("Could not find " + seat.Number, result.Value);
+        Assert.AreEqual( "Could not find " + 1, result.Value);
     }
 
     [TestMethod]
-    public void ReserveSeatBadRequest()
+    public void BadRequest()
     {
-        //mock du service (en premier pour pouvoir l'injecter dans le construteur du controlleur)
-        Mock<SeatsService> ServiceMock = new Mock<SeatsService>();
+        servicemock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new UserAlreadySeatedException());
 
-        //Mock du controller
-        Mock<SeatsController> ControllerMock = new Mock<SeatsController>(ServiceMock.Object) { CallBase = true };
-
-        //configuration de la propriete userid
-        ControllerMock.Setup(t => t.UserId).Returns("1");
-
-        //configuration de la reservation du seat pour throw une exception donc
-        //peut importe les parametre on veut juste voir si cv retourner Unauthorized à la fin
-        ServiceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new UserAlreadySeatedException());
-
-        //configuration de la reservation du seat avec le service 
-        var actionresult = ControllerMock.Object.ReserveSeat(1);
-
-        //resulat attendu
+        var actionresult = controllermock.Object.ReserveSeat(1);
         var result = actionresult.Result as BadRequestResult;
         Assert.IsNotNull(result);
-
     }
 }
